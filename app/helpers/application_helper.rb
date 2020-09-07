94badb4a3fd25b9ad5bbc9f5ce18155fc27d7066
Jeff Li <jeff.chun.yinli@gmail.com>
2020-09-07 14:46:05 -0400

    added up/down arrow to indicate sorted column

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def sortable(column, title)
     sort_order = params[:sort_order] == 'asc' ? 'desc' : 'asc'
 
-    link_to title, sort_column: column, sort_order: sort_order
+    link_to title, { sort_column: column, sort_order: sort_order }, { class: "sorted #{sort_order}" }
   end
 end
