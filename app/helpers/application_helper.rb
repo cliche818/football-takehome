3077ee269af5b452c0ba5d9c09a0883feb1317f5
Jeff Li <jeff.chun.yinli@gmail.com>
2020-09-07 16:03:36 -0400

    fixed player_name filter not working with sort order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,12 @@ module ApplicationHelper
     sort_order = params[:sort_order] == 'asc' ? 'desc' : 'asc'
     css_class = column == params[:sort_column] ? "sorted #{sort_order}" : ''
 
-    link_to title, { sort_column: column, sort_order: sort_order }, { class: css_class }
+    search_params = { sort_column: column, sort_order: sort_order }
+
+    if params[:player_name].present?
+      search_params.merge!(player_name: params[:player_name])
+    end
+
+    link_to title, search_params, { class: css_class }
   end
 end
