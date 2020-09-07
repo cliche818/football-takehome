3de9c70720e964a54439cbf39115460204667015
Jeff Li <jeff.chun.yinli@gmail.com>
2020-09-07 15:04:03 -0400

    added sorting to total_rushing_touchdowns and longest_rush + fix display arrow bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def sortable(column, title)
     sort_order = params[:sort_order] == 'asc' ? 'desc' : 'asc'
+    css_class = column == params[:sort_column] ? "sorted #{sort_order}" : ''
 
-    link_to title, { sort_column: column, sort_order: sort_order }, { class: "sorted #{sort_order}" }
+    link_to title, { sort_column: column, sort_order: sort_order }, { class: css_class }
   end
 end
