0e453e379cf7e8d6b0013c0da527637c921a539c
Jeff Li <jeff.chun.yinli@gmail.com>
2020-09-09 14:13:49 -0400

    change csv header to be the same as the json + don't send commit params when searching by name

--- app/models/player_rushing_stat.rb
+++ app/models/player_rushing_stat.rb
@@ -35,28 +35,42 @@ class PlayerRushingStat < ApplicationRecord
 
   def self.to_csv(player_rushing_stats)
     csv_columns = [
-        'player_name',
-        'player_team',
-        'player_position',
-        'rushing_attempts_per_game_avg',
-        'rushing_attempts',
-        'total_rushing_yards',
-        'rushing_average_yards_per_attempt',
-        'rushing_yards_per_game',
-        'total_rushing_touchdowns',
-        'longest_rush',
-        'longest_rush_with_touchdown',
-        'rushing_first_downs',
-        'rushing_first_down_percentage',
-        'rushing_twenty_plus_yards',
-        'rushing_forty_plus_yards',
-        'rushing_fumbles'
+      'Player',
+      'Team',
+      'Pos',
+      'Att',
+      'Att/G',
+      'Yds',
+      'Avg',
+      'Yds/G',
+      'TD',
+      'Lng',
+      '1st',
+      '1st%',
+      '20+',
+      '40+',
+      'FUM'
     ]
 
     CSV.generate do |csv|
       csv << csv_columns
       player_rushing_stats.each do |player_rushing_stat|
-        csv << player_rushing_stat.attributes.values_at(*csv_columns)
+        csv <<  [ player_rushing_stat.player_name,
+                  player_rushing_stat.player_team,
+                  player_rushing_stat.player_position,
+                  player_rushing_stat.rushing_attempts,
+                  player_rushing_stat.rushing_attempts_per_game_avg,
+                  player_rushing_stat.total_rushing_yards,
+                  player_rushing_stat.rushing_average_yards_per_attempt,
+                  player_rushing_stat.rushing_yards_per_game,
+                  player_rushing_stat.total_rushing_touchdowns,
+                  "#{player_rushing_stat.longest_rush}#{player_rushing_stat.longest_rush_with_touchdown ? 'T' : ''}",
+                  player_rushing_stat.rushing_first_downs,
+                  player_rushing_stat.rushing_first_down_percentage,
+                  player_rushing_stat.rushing_twenty_plus_yards,
+                  player_rushing_stat.rushing_forty_plus_yards,
+                  player_rushing_stat.rushing_fumbles
+        ]
       end
     end
   end
